f6aad7b4640714ee0c8209fdcd2c005c792b72bd
mehadi-shuvo <mehadihasanshuvo88@gamil.com>
2023-11-25 18:55:58 +0600

    interface and model basic done

---
+++ src/module/user/user.interface.ts
@@ -0,0 +1,20 @@
+export type TFullName = {
+  firstName: string;
+  lastName: string;
+};
+
+export type TUser = {
+  userId: number;
+  username: string;
+  password: string;
+  fullName: TFullName;
+  age: number;
+  email: string;
+  isActive: boolean;
+  hobbies: string[];
+  address: {
+    street: string;
+    city: string;
+    country: string;
+  };
+};
---
+++ src/module/user/user.model.ts
@@ -0,0 +1,21 @@
+import { Schema, model } from 'mongoose';
+import { TUser } from './user.interface';
+
+const userSchema = new Schema<TUser>({
+  userId: { type: Number, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  fullName: { type: String, required: true },
+  age: { type: Number, required: true },
+  email: { type: String, required: true },
+  isActive: { type: Boolean, required: true },
+  hobbies: { type: [String], required: true },
+  address: {
+    street: { type: String, required: true },
+    city: { type: String, required: true },
+    country: { type: String, required: true },
+  },
+});
+
+// model of a user data=====================
+export const User = model<TUser>('users', userSchema);
